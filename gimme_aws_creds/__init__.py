39509c2008819a8b36638f20a3ee43ca425d5a5d
Mark J. Reed <mark.reed@warnermedia.com>
2023-08-29 10:39:52 -0400

    fix: formatting

--- gimme_aws_creds/__init__.py
+++ gimme_aws_creds/__init__.py
@@ -1,2 +1,6 @@
-__all__ = ['config', 'aws', 'main', 'ui', 'common', 'default', 'duo', 'errors', 'okta_classic', 'okta_identity_engine', 'registered_authenticators', 'u2f', 'webauthn']
+__all__ = [
+  'config', 'aws', 'main', 'ui', 'common', 'default', 'duo', 'errors',
+  'okta_classic', 'okta_identity_engine', 'registered_authenticators', 'u2f',
+  'webauthn'
+]
 version = '2.7.2.1'
